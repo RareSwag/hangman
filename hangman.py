ad35c7dadedd1ed3f5c3d7e22af9962e2e759865
RareSwag <jheinl3766@greenvilleschools.us>
2017-11-10 09:23:02 -0500

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
+# Hangman, Jaden H.
+
 def get_puzzle():
-    pass
+    return "wowsers"
 
 def get_solved(puzzle, guesses):
     solved = ""
@@ -13,15 +15,42 @@ def get_solved(puzzle, guesses):
     return solved
 
 def get_guess():
-    pass
+    letter = input("Gimme a letter that might be in the word thank!")
+    return letter
 
 def display_board(solved):
-    pass
+    print(solved)
 
 def show_result():
-    pass
-    
+    print("Bruh Gj you're so cool")
+
+def check_strikes():
+    if strikes == limit:
+        
+    else:
 def play():
-    pass
-    
-play()
+    strikes = 0
+    limit = 6
+    puzzle = get_puzzle()
+    guesses = ""
+    solved = get_solved(puzzle, guesses)
+    display_board(solved)
+
+    while solved != puzzle:
+        guesses += get_guess()
+        solved = get_solved(puzzle, guesses)
+        show_result()
+        display_board(solved)
+
+def play_again():
+    again = input("Wanna go again bbbboi! (y/n)")
+    if again == "y":
+        return True
+    else:
+        return False
+
+
+y = True
+while y == True:
+    play()
+    y = play_again()
